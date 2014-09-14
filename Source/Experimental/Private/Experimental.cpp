1f281d7ca43286bf165ff980de584d33c0141f4a
Wymiatator <marekroefler@gmail.com>
2014-09-14 18:07:42 +0200

    Initial source

---
+++ Source/Experimental/Private/Experimental.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "Experimental.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Experimental, "Experimental" );
+
+DEFINE_LOG_CATEGORY(LogExperimental)
+ 
---
+++ Source/Experimental/Public/Experimental.h
@@ -0,0 +1,11 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __EXPERIMENTAL_H__
+#define __EXPERIMENTAL_H__
+
+#include "EngineMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogExperimental, Log, All);
+
+
+#endif
